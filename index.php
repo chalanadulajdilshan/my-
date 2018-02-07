60a59195bf3ec596dbf73c40c81faeab3e43a99f
chalanadulaj <chalanadulaj99@gmail.com>
2018-02-07 09:48:49 +0530

    Add Basic Html

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<?php
+<html>
+<body>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
 
+</body>
+</html>
